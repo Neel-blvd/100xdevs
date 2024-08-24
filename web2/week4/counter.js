2217d7d6120aaba3bf563392d638e9b899dcff15
Neel <neelphadke13579@gmail.com>
2024-08-24 21:09:24 +0530

    add week4

---
+++ web2/week4/counter.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const { Command } = require('commander');
+const program = new Command();
+
+program
+  .name('counter')
+  .description('CLI to do file based tasks')
+  .version('0.8.0');
+
+program.command('count')
+  .description('Count the number of lines in a file')
+  .argument('<file>', 'file to count')
+  .action((file) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+            console.log(data.split(/\s+/).filter((substr) => substr.length > 0).length)
+      }
+    });
+  });
+
+program.parse();
